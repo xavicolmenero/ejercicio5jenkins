e4fe5dcb13fe83421e939a2a68eb6404b1e523ca
xavicolmenero <67141230+xavicolmenero@users.noreply.github.com>
2023-05-31 12:21:01 +0200

    Add files via upload

---
+++ src/main/java/com/example/ejercicio5/ejercicio5/Ejercicio5Application.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio5.ejercicio5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejercicio5/ejercicio5/Ejercicio5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio5.ejercicio5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
